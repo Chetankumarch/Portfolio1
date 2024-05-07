19ca94f76e9e970f11720a1b32d883994f96c48b
Chetan Kumar CH <chetankumarch026@gmail.com>
2024-05-07 13:03:43 -0400

    profile content

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -13,13 +13,16 @@ export default function VortexDemo() {
         <img
           src="personal/myphoto.jpeg" // Replace with your image path
           alt="Descriptive Text"
-          className="mt-16 w-32 h-32 md:w-52 md:h-52 rounded-full object-cover"
+          className="mt-20 w-32 h-32 md:w-52 md:h-52 rounded-full object-cover"
         />
         <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
           Chetan Kumar Nageshwarrao Chitturi
         </h2>
         <p className="text-cyan-300 text-sm md:text-2xl w-full mt-6 text-center text-justify">
-        Empowering technological transformation and crafting cutting-edge solutions, I am a dedicated Software Developer with a robust educational background from Binghamton University and hands-on experience across global tech landscapes. Specializing in full-stack development, my journey through dynamic web applications, AI-driven enhancements, and system optimizations reflects a commitment to elevating user experiences and operational efficiencies. With a knack for adapting advanced technologies and a proven track record of boosting performance metrics, I am passionate about leveraging my skills in a forward-thinking environment that challenges the norm and values innovative problem-solving. Let's connect and explore how we can drive the future of technology together.
+        Empowering technological transformation and crafting cutting-edge solutions, I am a dedicated Software Developer with over three years of professional experience and a robust educational background from Binghamton University. My expertise spans full-stack development, where I have actively engaged in creating dynamic web applications, AI-driven enhancements, and system optimizations. This experience reflects a deep commitment to elevating user experiences and enhancing operational efficiencies. Skilled in adapting advanced technologies and with a proven track record of boosting performance metrics, I am passionate about leveraging my skills in a forward-thinking environment that challenges conventional norms and values innovative problem-solving.
+        </p>
+        <p className="text-slate-400 text-sm md:text-2xl w-full mt-2 text-center">
+        Let's connect and explore how we can drive the future of technology together.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-8 mt-6">
           <a href="https://github.com/Chetankumarch" target="_blank" rel="noopener noreferrer" className="text-white text-4xl">
